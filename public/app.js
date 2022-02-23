38345ab364e32459de86319b9b553ada72b0ca71
Mattia Cardile <mattiaca003@gmail.com>
2022-02-23 22:46:00 +0100

    Updated some styles and added map form input

--- public/app.js
+++ public/app.js
@@ -1,4 +1,38 @@
-function initMap()
-{
+const imageInput = document.getElementById('image');
+const latInput = document.getElementById('latitude');
+const lngInput = document.getElementById('longitude');
 
+function getCurrentPosition()
+{
+    return new Promise((resolve, reject) =>
+    {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 }
+
+imageInput.addEventListener('change', e =>
+{
+    const previewImg = document.querySelector('form .field.image .preview');
+    previewImg.src = URL.createObjectURL(imageInput.files[0]);
+    previewImg.classList.add('shown');
+});
+
+const map = L.map('map', { zoom: 1 });
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+getCurrentPosition().then(geo => 
+{
+    map.setView([geo.coords.latitude, geo.coords.longitude], 10);
+});
+
+map.addEventListener('click', e =>
+{
+    const { lat, lng } = e.latlng;
+    L.marker([lat, lng]).addTo(map);
+    latInput.value = lat;
+    lngInput.value = lng;
+});
+
+
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { randomUUID } from 'crypto';
 import multer from 'multer';
 import { db } from './database.js';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const ORIGIN = `https://localhost:${PORT}`;
 const app = express();
 
