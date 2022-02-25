6836f271d24d460dbaaa62712ed35f3226367362
Mattia Cardile <mattiaca003@gmail.com>
2022-02-25 23:24:17 +0100

    Removed useless spaces :)

--- database.js
+++ database.js
@@ -4,13 +4,13 @@ export const db = new sqlite3.Database('pollution-images.db');
 db.serialize(() =>
 {
     db.run(`CREATE TABLE IF NOT EXISTS images (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        fileName TEXT, 
-        mimeType TEXT, 
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        fileName TEXT,
+        mimeType TEXT,
         size INTEGER,
         dateCreated DATE,
-        description TEXT, 
-        latitude FLOAT, 
+        description TEXT,
+        latitude FLOAT,
         longitude FLOAT
     )`);
 });
