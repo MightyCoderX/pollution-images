4c481e6a1359a313ffb948d2e3c7dd80f20aa343
Mattia Cardile <mattiaca003@gmail.com>
2022-02-26 00:12:32 +0100

    Fixed date format to be unix when an image is created

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/api/images/new', upload.single('image'), (req, res) =>
         fileName: '/' + newPath,
         mimeType: req.file.mimetype,
         size: req.file.size,
-        dateCreated: new Date(),
+        dateCreated: Date.now(),
         description: req.body.description,
         latitude: req.body.latitude,
         longitude: req.body.longitude
